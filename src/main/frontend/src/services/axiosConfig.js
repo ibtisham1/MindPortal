abac6d811a393e2533a9dd0a031082d0fee29477
MattB1 <matthew_bubb@hotmail.com>
2021-09-22 18:49:28 -0400

    updating build.

--- src/main/frontend/src/services/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-// replace this baseURL with environment variable in future.
-const instance = axios.create({
-    baseURL: 'http://localhost:8080'
-})
-
-export default instance;
