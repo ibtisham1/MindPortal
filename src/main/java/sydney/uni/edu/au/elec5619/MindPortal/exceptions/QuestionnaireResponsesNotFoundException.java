bfe5295c8d3792a5acdc383e2ecd8a8f5599df16
Kurt Lu <falu5003@uni.sydney.edu.au>
2021-09-23 23:25:16 +1000

    questionnaireResponses database schema setup finish

---
+++ src/main/java/sydney/uni/edu/au/elec5619/MindPortal/exceptions/QuestionnaireResponsesNotFoundException.java
@@ -0,0 +1,7 @@
+package sydney.uni.edu.au.elec5619.MindPortal.exceptions;
+
+public class QuestionnaireResponsesNotFoundException extends RuntimeException{
+    public QuestionnaireResponsesNotFoundException(String message) {
+        super(message);
+    }
+}
