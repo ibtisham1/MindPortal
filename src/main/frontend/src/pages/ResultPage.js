3a1c903be071d780140ed7b18013855958af4cbb
Eric Song <eson8088@uni.sydney.edu.au>
2021-09-29 13:42:09 +1000

    adjust spacing, add card styling

--- src/main/frontend/src/pages/ResultPage.js
+++ src/main/frontend/src/pages/ResultPage.js
@@ -1,24 +1,53 @@
 import React from "react";
 import { useAuth } from "../services/useAuth";
 import Header from "../components/Header";
-import {Col, Container, Row,Carousel} from "react-bootstrap";
+import {Col, Container, Row,Carousel, Card} from "react-bootstrap";
 import "../styles/Result.scss";
 
 
 const ResultPage = () => {
 
-
     return (
         <div>
             <Header />
-            <Container>
+            <Container fluid className="Result page">
                 <Row>
                     <Col className="Result result">
-                        <h1>Your result is well.</h1>
-                        <p>What does my score mean?</p>
-                        <p>Suggestion</p>
+                        <h2>Your result is well.</h2>
+                        <Card className="Result card">
+                            <Card.Body>
+                                <Card.Title>
+                                    What does my score mean?
+                                </Card.Title>
+                                <Card.Text>
+                                    Description of the K10 result
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <h5>Suggestions</h5>
+                        <Card className="Result card">
+                            <Card.Body>
+                                <Card.Title>
+                                    Suggestions
+                                </Card.Title>
+                                <Card.Text>
+                                    Type of Suggestions
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card className="Result card">
+                            <Card.Body>
+                                <Card.Title>
+                                    Suggestions
+                                </Card.Title>
+                                <Card.Text>
+                                    Type of Suggestions
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
                     <Col className="Result video">
+                        <h2>We are here to help you!</h2>
                         <Carousel interval={null}>
                             <Carousel.Item>
                                 <iframe width="100%" height="315" src = "https://www.youtube.com/embed/XyNlqQId-nk"
