bc3a69b26995e12a4e8b046746cf6ca647863a34
Ibtisham <ihaq6324@uni.sydney.edu.au>
2021-10-01 21:40:41 +1000

    Fixed the right column

--- src/main/frontend/src/pages/TestPage.js
+++ src/main/frontend/src/pages/TestPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth } from "../services/useAuth";
 import Header from "../components/Header";
-import {Col, Container, Row,Carousel} from "react-bootstrap";
+import {Col, Container, Row,Carousel, Button} from "react-bootstrap";
 import { useState } from "react";
 import ReactDOM from 'react-dom';
 import "../styles/TestForm.css";
@@ -10,7 +10,7 @@ import "../styles/TestForm.css";
 
 
 const TestPage = () => {
-    const preSet = [[
+    const questionIDs = [[
         { id: 1, name: "None of the time", isChecked: false },
         { id: 2, name: "A little of the time", isChecked: false },
         { id: 3, name: "Some of the time", isChecked: false },
@@ -63,16 +63,16 @@ const TestPage = () => {
             { id: 4, name: "Most of the time", isChecked: false },
             { id: 5, name: "All of the time", isChecked: false }]];
 
-    const [data, setData] = useState(preSet);
+    const [answers, setAnswers] = useState(questionIDs);
 
-    function handleChange(e, num) {
+    function record(e, num) {
         const value = e.target.value;
-        const modifiedData = [...data];
-        modifiedData[num].map((item) => {
+        const newAnswers = [...answers];
+        newAnswers[num].map((item) => {
             item.isChecked = item.id === +value;
             return item;
         });
-        setData(modifiedData);
+        setAnswers(newAnswers);
     }
 
     return (
@@ -87,12 +87,12 @@ const TestPage = () => {
 
 
                 <h4>About how often did you feel tired out for no good reason?</h4>
-                {data[0].map((item, index) => (
+                {answers[0].map((item, index) => (
                     <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e, 0)}
+                            onChange={(e) => record(e, 0)}
                             checked={item.isChecked}
                         />
                         {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -103,12 +103,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel nervous?</h4>
-                    {data[1].map((item, index) => (
+                    {answers[1].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e, 1)}
+                            onChange={(e) => record(e, 1)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -118,12 +118,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel so nervous that nothing could calm you down?</h4>
-                    {data[2].map((item, index) => (
+                    {answers[2].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e,2)}
+                            onChange={(e) => record(e,2)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -133,12 +133,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel hopeless?</h4>
-                    {data[3].map((item, index) => (
+                    {answers[3].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e,3)}
+                            onChange={(e) => record(e,3)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -148,12 +148,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel restless or fidgety?</h4>
-                    {data[4].map((item, index) => (
+                    {answers[4].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e,4)}
+                            onChange={(e) => record(e,4)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -163,12 +163,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel so restless you could not sit still?</h4>
-                    {data[5].map((item, index) => (
+                    {answers[5].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e,5)}
+                            onChange={(e) => record(e,5)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -178,12 +178,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel depressed?</h4>
-                    {data[6].map((item, index) => (
+                    {answers[6].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e,6)}
+                            onChange={(e) => record(e,6)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -193,12 +193,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel that everything was an effort?</h4>
-                    {data[7].map((item, index) => (
+                    {answers[7].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e,7)}
+                            onChange={(e) => record(e,7)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -208,12 +208,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel so sad that nothing could cheer you up?</h4>
-                    {data[8].map((item, index) => (
+                    {answers[8].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e,8)}
+                            onChange={(e) => record(e,8)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -223,12 +223,12 @@ const TestPage = () => {
                 </row>
                 <row>
                     <h4>About how often did you feel worthless?</h4>
-                    {data[9].map((item, index) => (
+                    {answers[9].map((item, index) => (
                         <span key={item.id}>
                         <input
                             type="Checkbox"
                             value={item.id}
-                            onChange={(e) => handleChange(e,9)}
+                            onChange={(e) => record(e,9)}
                             checked={item.isChecked}
                         />
                             {item.name}  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
@@ -236,12 +236,17 @@ const TestPage = () => {
                     </span>
                     ))}
                 </row>
-                <button>
-                    Submit
-                </button>
+                <row>
+                    <Container>
+                        <center>
+                            <bt><Button as="input" type="submit" value="Submit" />{' '}</bt>
+                        </center>
+                        </Container>
+                </row>
             </Col>
                     <Col xs={2} style={{backgroundColor: 'black'}}>
                         <h3> What is the K10 Test </h3>
+                            <st><a href="https://www.tac.vic.gov.au/files-to-move/media/upload/k10_english.pdf" target="_blank">Learn more here</a></st>
                     </Col>
                 </Row>
         </div>
