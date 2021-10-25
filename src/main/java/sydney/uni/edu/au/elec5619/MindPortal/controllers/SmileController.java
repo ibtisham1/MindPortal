2e2ef758abd28fa1b70c1db15a581ee3e5fa7692
MattB1 <matthew_bubb@hotmail.com>
2021-10-25 14:48:34 -0400

    code cleanup

--- src/main/java/sydney/uni/edu/au/elec5619/MindPortal/controllers/SmileController.java
+++ src/main/java/sydney/uni/edu/au/elec5619/MindPortal/controllers/SmileController.java
@@ -29,13 +29,11 @@ public class SmileController {
     @Autowired
     UserRepository userRepo;
 
-   
+
     @PostMapping(value = "/getResult")
     public ResponseEntity<?> getResult(@RequestBody byte[] byteArray, @PathVariable("id") Integer id) {
 
         String key = "6e4ccc74ec694dce89968a7db92f9660";
-        String url = "https://mindportal.cognitiveservices.azure.com/face/v1.0/detect?returnFacedId=true&returnFaceAttributes=smile,age,gender";
-
         HttpPost httpPost = new HttpPost("https://mindportal.cognitiveservices.azure.com/face/v1.0/detect");
 
         URI uri = null;
@@ -47,82 +45,43 @@ public class SmileController {
             e.printStackTrace();
         }
 
-//        HttpClient client = HttpClient.newHttpClient();
-//
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(uri)
-//                .header("Content-Type", " application/octet-stream")
-//                .header("Ocp-Apim-Subscription-Key", key)
-//                .POST(HttpRequest.BodyPublishers.ofByteArray(byteArray))
-//                .build();
-
-        // New post
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/octet-stream");
         headers.set("Ocp-Apim-Subscription-Key", key);
 
 
-
-
         try {
-//            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//            System.out.println(response);
-//            System.out.println(response.body());
 
-            // New
             HttpEntity<byte[]> entity = new HttpEntity<byte[]>(byteArray, headers);
-//            FaceAPIResponse faceAPIResponse = restTemplate.postForObject(url, entity, FaceAPIResponse.class, params);
             FaceAPIValues[] faceAPIValues = restTemplate.postForObject(uri, entity, FaceAPIValues[].class);
 
-            if (faceAPIValues != null) {
+            if (faceAPIValues != null && faceAPIValues.length != 0) {
                 FaceAPIValues result = faceAPIValues[0];
-                for(FaceAPIValues val :faceAPIValues){
-                    if (result != null) {
-                        if (result.getFaceId() != null) {
-                            System.out.println("id" + result.getFaceId());
-                        }
-                        if (result.getFaceAttributes() != null) {
-                            FaceAttributes faceAttributes = result.getFaceAttributes();
-                            if(faceAttributes != null){
-                                System.out.println("smile score: " + faceAttributes.getSmile());
-                                double score = faceAttributes.getSmile();
-
-                                // if score > 85 add to user
-                                if(score > 0.85){
-                                    // find user and set most recent date to e erg
-                                    User user = userRepo.findById(id).get();
-                                    user.setMostRecentSmileChallengePass(new Timestamp(System.currentTimeMillis()));
-                                    userRepo.save(user);
-                                }
-
-                                SmileResponse smileResponse = new SmileResponse(score);
-
-
-                                return new ResponseEntity<>(smileResponse, HttpStatus.OK);
+                if (result != null) {
+                    if (result.getFaceAttributes() != null) {
+                        FaceAttributes faceAttributes = result.getFaceAttributes();
+                        if (faceAttributes != null) {
+                            System.out.println("smile score: " + faceAttributes.getSmile());
+                            double score = faceAttributes.getSmile();
+                            // if score > 85 add to user
+                            if (score >= 0.85) {
+                                // find user and set most recent date to e erg
+                                User user = userRepo.findById(id).get();
+                                user.setMostRecentSmileChallengePass(new Timestamp(System.currentTimeMillis()));
+                                userRepo.save(user);
                             }
-                        }
 
+                            SmileResponse smileResponse = new SmileResponse(score);
+                            return new ResponseEntity<>(smileResponse, HttpStatus.OK);
+                        }
                     }
                 }
             }
-//            FaceAttributes faceAttributes = restTemplate.postForObject(url, entity, FaceAttributes.class, params);
-
-//            if(faceAttributes != null){
-//                System.out.println("post for Object worked");
-//                System.out.println("smile score: " + faceAttributes.getSmile());
-//            }
-
-            return new ResponseEntity<>("okay", HttpStatus.OK);
-//            return new ResponseEntity<>(response.body(), HttpStatus.OK);
-        } catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
 
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
