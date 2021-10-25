c2e46c785af828fa033f0601a2f9888aa916f0e5
MattB1 <matthew_bubb@hotmail.com>
2021-10-25 15:02:50 -0400

    add frontend check for past day on smile challenge

--- src/main/frontend/src/components/ProfileTabs/ProfileDetailsTab.js
+++ src/main/frontend/src/components/ProfileTabs/ProfileDetailsTab.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Container, Row, Col } from "react-bootstrap";
 import "../../styles/Profile.scss";
@@ -6,6 +6,25 @@ import { BsPersonCircle } from "react-icons/bs";
 
 const ProfileDetailsTab = (props) => {
     console.log(props.user);
+
+    const [completedSmileChallenge, setCompletedSmileChallenge] =
+        useState(false);
+
+    useEffect(() => {
+        let user = props.user;
+
+        let lastSmile = user.mostRecentSmileChallengePass;
+        if (lastSmile != null) {
+            // compute diff
+
+            let diff = new Date() - new Date(lastSmile);
+            console.log(diff);
+            if (diff < 86400000) {
+                setCompletedSmileChallenge(true);
+            }
+        }
+    }, []);
+
     return (
         <Container className="details">
             <Row>
@@ -36,6 +55,16 @@ const ProfileDetailsTab = (props) => {
                             Email
                         </Col>
                         <Col className="details__value">{props.user.email}</Col>
+                    </Row>
+                    <Row className="details__row">
+                        <Col sm={3} className="details__label">
+                            Daily Smile Challenge
+                        </Col>
+                        <Col className="details__value">
+                            {completedSmileChallenge
+                                ? "has completed in past day"
+                                : "not not completed - try a smile challenge"}
+                        </Col>
                     </Row>
                 </Col>
             </Row>
