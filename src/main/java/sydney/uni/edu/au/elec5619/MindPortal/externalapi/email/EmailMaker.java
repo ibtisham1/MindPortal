c4fab6bca70b3cd0e45c2cbf6a19d8923b050adc
sima3098 <sima3098@uni.sydney.edu.au>
2021-10-27 01:34:30 +1100

    Added jackson mapper of the request body

--- src/main/java/sydney/uni/edu/au/elec5619/MindPortal/externalapi/email/EmailMaker.java
+++ src/main/java/sydney/uni/edu/au/elec5619/MindPortal/externalapi/email/EmailMaker.java
@@ -1,2 +1,15 @@
-package sydney.uni.edu.au.elec5619.MindPortal.externalapi.email;public class EmailMaker {
+package sydney.uni.edu.au.elec5619.MindPortal.externalapi.email;
+
+public class EmailMaker {
+
+    private String senderEmail;
+    private String emailBody;
+
+    public String getSenderEmail() {
+        return senderEmail;
+    }
+
+    public String getEmailBody() {
+        return emailBody;
+    }
 }
