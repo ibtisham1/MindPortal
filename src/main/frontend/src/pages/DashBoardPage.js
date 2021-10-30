696850cf585eade12ea298de692cc6dec17fb629
MattB1 <matthew_bubb@hotmail.com>
2021-10-30 15:14:14 -0400

    removing fixed width on cards.

--- src/main/frontend/src/pages/DashBoardPage.js
+++ src/main/frontend/src/pages/DashBoardPage.js
@@ -16,7 +16,7 @@ import {
 } from "react-bootstrap";
 import Header from "../components/Header";
 import axios from "axios";
-import "../styles/DashBoard.scss";
+import "../styles/Dashboard.scss";
 
 const DashBoardPage = () => {
     let location = useLocation();
@@ -58,7 +58,13 @@ const DashBoardPage = () => {
         return (
             <Card style={{ borderRadius: 10, marginTop: 5 }}>
                 <Card.Body>
-                    <Card.Title style={{ color: "rgba(242,109,113,1)" ,textAlign: "center" }} className="mindPortalAlert">
+                    <Card.Title
+                        style={{
+                            color: "rgba(242,109,113,1)",
+                            textAlign: "center",
+                        }}
+                        className="mindPortalAlert"
+                    >
                         New Covid Venue Alert
                     </Card.Title>
                     <p style={{ marginBottom: 0 }}>
