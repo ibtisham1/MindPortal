b4c4571bcdc5e3bac6efb29603cf48ba96ff7c80
Kurt Lu <falu5003@uni.sydney.edu.au>
2021-10-31 19:58:49 +1100

    fixed up result page

--- src/main/frontend/src/pages/ResultPage.js
+++ src/main/frontend/src/pages/ResultPage.js
@@ -38,30 +38,30 @@ const ResultPage = () => {
 
         let paragraphOutput;
         if (answer=="well"){
-            paragraphOutput="The diagnosis states that your result is well.\n" +
+            paragraphOutput=
                 "Please enjoy the calming tunes in your Moments-to-Appreciate page." +
                 "And if you are ever feeling distressed, feel free to take the test again.";
 
         }
         else if (answer=="mild"){
-            paragraphOutput="The diagnosis states that your result is mild.\n" +
+            paragraphOutput=
                 "We see that you mild levels of anxiety and/or depression." +
                 "Please enjoy the calming tunes in yours Moments-to-Appreciate page." +
                 "We hope the soothing tunes will help you out.";
 
         }
         else if (answer=="moderate"){
-            paragraphOutput="The diagnosis states that your result is moderate.\n" +
+            paragraphOutput=
                 "We see that you moderate levels of anxiety and/or depression." +
-                "We recommend you to seek Professional help in the link given below." +
+                "We recommend you to seek Professional help." +
                 "Also please feel free to listen to the calming tunes in your Moments-to-Appreciate page " +
                 "to help ease the distress";
 
         }
         else if (answer=="severe"){
-            paragraphOutput="The diagnosis states that your result is severe.\n" +
+            paragraphOutput=
                 "We see that you severe levels of anxiety and/or depression." +
-                "We recommend you to to seek Professional help in the link given below, and call the Suicide Prevention Hotline if necessary. " +
+                "We recommend you to to seek Professional help, and call the Suicide Prevention Hotline if necessary (131114). " +
                 "Also please feel free to listen to the calming tunes in your Moments-to-Appreciate page " +
                 "to help ease the distress";
 
@@ -79,28 +79,29 @@ const ResultPage = () => {
             <Container fluid className="Result page">
                 <Row>
                     <Col className="Result result">
-                        <h2>{getAnswer()}</h2>
+                        {/*<h2>{getAnswer()}</h2>*/}
+                        <h2>The diagnosis states that your result is {rD.getDiagnosis()}</h2>
                         <Card className="Result card">
                             <Card.Body>
                                 <Card.Title>
                                     What does my score mean?
                                 </Card.Title>
                                 <Card.Text>
-                                    Description of the K10 result
+                                    {getAnswer()}
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                         <h5>Suggestions</h5>
-                        <Card className="Result card">
-                            <Card.Body>
-                                <Card.Title>
-                                    Suggestions
-                                </Card.Title>
-                                <Card.Text>
-                                    Type of Suggestions
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        {/*<Card className="Result card">*/}
+                        {/*    <Card.Body>*/}
+                        {/*        <Card.Title>*/}
+                        {/*            Suggestions*/}
+                        {/*        </Card.Title>*/}
+                        {/*        <Card.Text>*/}
+                        {/*            Type of Suggestions*/}
+                        {/*        </Card.Text>*/}
+                        {/*    </Card.Body>*/}
+                        {/*</Card>*/}
                         <Card className="Result card">
                             <Card.Body>
                                 <Card.Title>
